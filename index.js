d344e1d1ba21802123d03530065dc328a5b34478
floydpadua <134394148+FloydPadua@users.noreply.github.com>
2024-05-25 10:30:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,25 @@ function countLike2() {
 }
 btnLike2.addEventListener("click", countLike2)
 
+
+const btnDislike1 = document.getElementById("btnDislike1")
+const count3 = document.getElementById("count3")
+
+function countDislike1() {
+  let DislikeCounts = parseInt(count3.value) + 1
+  count3.textContent = DislikeCounts.toString()
+}
+btnDislike1.addEventListener("click", countDislike1)
+
+
+
+
+const btnDislike2 = document.getElementById("btnDislike2")
+const count4 = document.getElementById("count4")
+
+function countDislike2() {
+  let DislikeCounts = parseInt(count4.value) + 1
+  count4.textContent = DislikeCounts.toString()
+}
+btnDislike2.addEventListener("click", countDislike2)
+

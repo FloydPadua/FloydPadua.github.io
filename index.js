4af68b03bdac640567fa5585d731aaea8705abd9
floydpadua <134394148+FloydPadua@users.noreply.github.com>
2024-07-02 20:58:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -79,8 +79,7 @@ var cash = document.getElementById("cash")
 var change = document.getElementById("change")
 
 function addOrder() {
-    var orders = "";
-    var totalAmount = 0;
+    carts.textContent=""
 
     if (parseFloat(qty1.value) > 0) {
         var order1 = qty1.value + " pc/s x " + price1.textContent + " - " + product1.textContent + " - Php " + (parseFloat(qty1.value) * parseFloat(price1.textContent)).toFixed(2) + "\n";
@@ -117,43 +116,9 @@ function addOrder() {
         orders += order6;
         totalAmount += parseFloat(qty6.value) * parseFloat(price6.textContent);
     }
-
-    carts.textContent = orders;
-    total.value = "Php " + totalAmount.toFixed(2);
 }
 
-function calculateChange() {
-    var totalAmount = parseFloat(total.value.replace("Php ", ""));
-    var cashAmount = parseFloat(cash.value);
-    if (cashAmount >= totalAmount) {
-        change.value = "Php " + (cashAmount - totalAmount).toFixed(2);
-    } else {
-        change.value = "Insufficient Cash";
-    }
-}
 
-function finishOrder() {
-    var totalAmount = parseFloat(total.value.replace("Php ", ""));
-    var cashAmount = parseFloat(cash.value);
-
-    if (cashAmount >= totalAmount && totalAmount > 0) {
-        alert("Order Successful!\n\n" + "Products Purchased:\n" + carts.textContent + "\nHAVE A GOOD DAY!");
-        carts.textContent = "";
-        total.value = "";
-        cash.value = "";
-        change.value = "";
-        qty1.value = "";
-        qty2.value = "";
-        qty3.value = "";
-        qty4.value = "";
-        qty5.value = "";
-        qty6.value = "";
-    } else if (totalAmount == 0) {
-        alert("Please add items to your order.");
-    } else {
-        alert("Insufficient Cash.");
-    }
-}
 
 qty1.addEventListener("keyup", addOrder);
 qty2.addEventListener("keyup", addOrder);
@@ -161,6 +126,6 @@ qty3.addEventListener("keyup", addOrder);
 qty4.addEventListener("keyup", addOrder);
 qty5.addEventListener("keyup", addOrder);
 qty6.addEventListener("keyup", addOrder);
-cash.addEventListener("keyup", calculateChange);
+
 
 
